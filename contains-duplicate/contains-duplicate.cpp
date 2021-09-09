b030bb37e112e0c11414c3700f4e958567f380e6
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2021-09-09 10:47:25 +0530

    Time: 55 ms (12.48%), Space: 15.6 MB (72.61%) - LeetHub

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -3,16 +3,16 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         
     sort(nums.begin(),nums.end());
-    bool dupli=false;
-    int idx=1;
+   
+ 
     for(int i=1;i<nums.size();i++)
     {
         if(nums[i-1]==nums[i])
         { 
-            dupli=true;
-            idx++;
+            return true;
+            
         }
     }
-    return dupli;       
+    return false;       
     }
 };
