d5b02cd46276e43abab9c1499197417f638a6929
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2021-09-11 20:35:10 +0530

    Time: 45 ms (16.50%), Space: 25 MB (72.65%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-/*We can use two pointers, say i and j which point to first and last element of array respectively. As we know in cyclic rotation we will bring last element to first and shift rest in forward direction, so start swaping arr[i] and arr[j] and keep j fixed and i moving towards j.  Repeat till i is not equal to j.*/
-        k=k%nums.size();
-           reverse(nums.begin(),nums.begin()+nums.size()-k);
-            reverse(nums.begin()+nums.size()-k,nums.end());
-        reverse(nums.begin(),nums.end());
+        k %=nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
