f7abc40ebd3c7fd6c10ae78f35516d7d7908a76c
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2021-09-30 22:01:43 +0530

    add comments

--- letter-case-permutation/letter-case-permutation.cpp
+++ letter-case-permutation/letter-case-permutation.cpp
@@ -8,16 +8,16 @@ public:
     }
     
     void solve(string s,int i){
-         if(s.size()==i){
+         if(s.size()==i){ //base case
             ans.push_back(s);
             return;
         }
         
         if(isalpha(s[i])){
-            s[i]=toupper(s[i]);
-            solve(s,i+1);
-            s[i]=tolower(s[i]);
-            solve(s,i+1);
+            s[i]=toupper(s[i]); // sub case 1, considering upper case
+            solve(s,i+1); // Append toupper(s[i]) to s[i] and go to next index (i+1).
+            s[i]=tolower(s[i]); //sub case 2, considering lower case
+            solve(s,i+1); // Append tolower(s[i]) to s[i] and go to next index (i+1).
         }
         else{
             solve(s,i+1);
