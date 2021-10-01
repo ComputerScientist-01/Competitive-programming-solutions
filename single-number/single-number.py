1cd86d7fc22d011ef9cde19314b763d1b28975f4
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2021-10-01 08:20:12 +0530

    Time: 92 ms (99.82%), Space: 15.9 MB (48.39%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,4 +1,8 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        return 2*sum(set(nums))-sum(nums)
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 2 * sum(set(nums)) - sum(nums)
         
